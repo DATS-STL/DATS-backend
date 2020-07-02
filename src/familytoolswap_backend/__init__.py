ff58a9a22903b0dbd3880b22b1d04a93901fe438
Alex Gittemeier <gittemeier.alex@gmail.com>
2020-07-02 13:05:16 -0500

    Import app instead of having it be passed in

--- src/familytoolswap_backend/__init__.py
+++ src/familytoolswap_backend/__init__.py
@@ -5,8 +5,6 @@ Copyright 2020, Alex Gittemeier, Tom Gittemeier, Dan Telle, Steve Telle
 Licensed under GPLv3.0.
 '''
 from flask import Flask
-from . import routes
-
 app = Flask(__name__)
 
-routes.setup(app)
+from . import routes
--- src/familytoolswap_backend/routes.py
+++ src/familytoolswap_backend/routes.py
@@ -3,8 +3,9 @@ Copyright 2020, Alex Gittemeier, Tom Gittemeier, Dan Telle, Steve Telle.
 Licensed under GPLv3.0.
 '''
 
-def setup(app):
-	# a simple page that says hello
-	@app.route('/hello')
-	def hello():
-			return 'Hello, World!'
+from . import app
+
+# a simple page that says hello
+@app.route('/hello')
+def hello():
+		return 'Hello, World!'

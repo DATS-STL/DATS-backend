146f3ed7da0fe66f4171abe1e7592d39193b6b24
Alex Gittemeier <gittemeier.alex@gmail.com>
2020-07-07 18:33:35 -0500

    Add DB connection code and endpoint to return welcome message

--- src/familytoolswap_backend/__init__.py
+++ src/familytoolswap_backend/__init__.py
@@ -5,6 +5,14 @@ Copyright 2020, Alex Gittemeier, Tom Gittemeier, Dan Telle, Steve Telle
 Licensed under GPLv3.0.
 '''
 from flask import Flask
+from . import environment
+import psycopg2
+
 app = Flask(__name__)
+postgres = psycopg2.connect(
+		host=    environment.POSTGRES_HOST,
+		user=		 environment.POSTGRES_USER,
+		password=environment.POSTGRES_PASSWORD,
+		database=environment.POSTGRES_DATABASE)
 
 from . import routes
--- src/familytoolswap_backend/routes.py
+++ src/familytoolswap_backend/routes.py
@@ -3,7 +3,14 @@ Copyright 2020, Alex Gittemeier, Tom Gittemeier, Dan Telle, Steve Telle.
 Licensed under GPLv3.0.
 '''
 
-from . import app
+from . import app, postgres
+
+@app.route('/')
+def get_root():
+	cursor = postgres.cursor()
+	cursor.execute("SELECT value FROM messages WHERE name = 'welcome'")
+	value, = cursor.fetchone()
+	return value
 
 # a simple page that says hello
 @app.route('/hello')

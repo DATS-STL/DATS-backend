dfe12ab2b23ddd3acafe739d416027d6d5df920f
Steven Telle <stevetelle@hotmail.com>
2020-08-06 20:19:55 -0500

    Get and post user mostly working

--- src/familytoolswap_backend/models/user.py
+++ src/familytoolswap_backend/models/user.py
@@ -3,5 +3,5 @@ from .. import app, sa
 class User(sa.Model):
 	__tablename__ = "user"
 	user_id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
-	username = sa.Column(sa.Integer, nullable=False)
+	username = sa.Column(sa.String(255), nullable=False)
 	password = sa.Column(sa.String(255), nullable=False)
